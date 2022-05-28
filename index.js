ce7255fb9bfb3137237453c86d434e42a561a8a0
Darin Pope <darin@planetpope.com>
2022-05-27 22:05:53 -0400

    next big work

--- index.js
+++ index.js
@@ -96,102 +96,133 @@ instance.prototype.actions = function(system) {
 
 instance.prototype.action = function(action) {
 	var self = this;
-	var notionUrl = '';
-	var body = {};
-	var extra_headers = [];
-	extra_headers['Authorization'] = 'Bearer ' + self.config.apiKey;
-	extra_headers['Notion-Version'] = '2022-02-22';
-	var rightNow = Date.now();
-	var makeRestCall = false;
+	const rightNow = Date.now();
+	const isoDate = new Date(rightNow).toISOString();
 	
 	switch(action.action) {
 		case 'start':
-			makeRestCall = true;
-			notionUrl = 'https://api.notion.com/v1/databases/';
-			body = JSON.stringify({
-				parent: {
-					type:"page_id",
-					page_id: self.config.parentPageId,
-				},
-				title:[{
-					type:"text",
-					text:{
-						content:"TestDB-"+rightNow
-					}
-				}],
-				properties: {
-					name: {
-						title:{}
-					},
-					description: {
-						rich_text:{}
-					},
-					companionTime: {
-						number:{}
-					},
-					elapsedTime: {
-						number:{}
-					},
-					createTime: {
-						created_time:{}
-					}
-				}
-			});
+			self.startSession(rightNow, isoDate);
 			break;
 		case 'stop':
-			self.NOTIONINFO.active = false;
-			self.NOTIONINFO.databaseId = '';
-			self.NOTIONINFO.startTime = 0;
+			self.stopSession();
 			break;
 		case 'createMarker':
-			makeRestCall = true;
-			notionUrl = 'https://api.notion.com/v1/pages';
-			body = JSON.stringify({
-				parent: {
-					database_id: self.NOTIONINFO.databaseId
-				},
-				properties: {
-					name: {
-						title:[{
-							text: {
-								content:"This is a name"
-							}
-						}],
-					},
-					description: {
-						rich_text:[{
-							text: {
-								content:"This is a description"
-							}
-						}],
-					},
-					companionTime: {
-						number: rightNow
-					},
-					elapsedTime: {
-						number: rightNow - self.NOTIONINFO.startTime
-					}
-				}
-			});
+			self.createMessage(rightNow, isoDate, action.options.message);
 			break;
 		default:
 			break;
 	};
+}
 
-	console.log(self.NOTIONINFO);
+instance.prototype.createMessage = function(rightNow, isoDate, message) {
+	let self = this;
 
-	if(makeRestCall === false) {
-		console.log("rest is false");
-		return;
-	};
+	const elapsedTime = rightNow - self.NOTIONINFO.startTime;
+	const totalSeconds = Math.round(elapsedTime / 1000);
+	const hours = Math.floor(totalSeconds / 3600);
+	const minutes = Math.floor(totalSeconds / 60);
+	const seconds = totalSeconds % 60;
+	let timestampFmt = "";
+	if(hours !== 0) {
+		timestampFmt = hours.toString().padStart(2,'0')+":"+minutes.toString().padStart(2,'0')+":"+seconds.toString().padStart(2,'0');		
+	} else {
+		timestampFmt = minutes.toString().padStart(2,'0')+":"+seconds.toString().padStart(2,'0');
+	}
+
+	body = JSON.stringify({
+		parent: {
+			database_id: self.NOTIONINFO.databaseId
+		},
+		properties: {
+			message: {
+				title:[{
+					text: {
+						content:message
+					}
+				}],
+			},
+			companionTimeMillis: {
+				number: rightNow
+			},
+			companionTimeDate: {
+				rich_text:[{
+					text: {
+						content: isoDate
+					}
+				}]
+			},
+			elapsedTime: {
+				number: elapsedTime
+			},
+			timestampValue: {
+				rich_text:[{
+					text: {
+						content: timestampFmt
+					}
+				}]
+			}
+		}
+	});
+	self.doRestCall('https://api.notion.com/v1/pages',body, rightNow, isoDate);
+}
+
+instance.prototype.startSession = function(rightNow, isoDate) {
+	let self = this;
+
+	body = JSON.stringify({
+		parent: {
+			type:"page_id",
+			page_id: self.config.parentPageId,
+		},
+		title:[{
+			type:"text",
+			text:{
+				content:isoDate
+			}
+		}],
+		properties: {
+			message: {
+				title:{}
+			},
+			companionTimeMillis: {
+				number:{}
+			},
+			companionTimeDate: {
+				rich_text:{}
+			},
+			elapsedTime: {
+				number:{}
+			},
+			timestampValue: {
+				rich_text:{}
+			},
+			createTime: {
+				created_time:{}
+			}
+		}
+	});
+	self.doRestCall('https://api.notion.com/v1/databases/', body, rightNow, isoDate);
+}
+
+instance.prototype.stopSession = function() {
+	let self = this;
+	self.NOTIONINFO.active = false;
+	self.NOTIONINFO.databaseId = '';
+	self.NOTIONINFO.startTime = 0;
+}
+
+instance.prototype.doRestCall = function(notionUrl, body, rightNow, isoDate) {
+	let self = this;
+
+	var extra_headers = [];
+	extra_headers['Authorization'] = 'Bearer ' + self.config.apiKey;
+	extra_headers['Notion-Version'] = '2022-02-22';
 
 	self.system.emit('rest', notionUrl, body, function (err, result) {
-		//console.log(result);
 		if (err !== null) {
 			self.log('error', 'Notion Send Failed (' + result.error.code + ')');
 			self.status(self.STATUS_ERROR, result.error.code);
 		} else if(result.data.object === 'error') {
-			//console.log(result.data.object);
 			self.log('error',result.data.code + ' ' + result.data.message)
 			self.status(self.STATUS_ERROR, result.data.status);
 		} else if(result.data.object === 'database') {
@@ -199,8 +230,8 @@ instance.prototype.action = function(action) {
 			self.NOTIONINFO.databaseId = result.data.id;
 			self.NOTIONINFO.startTime = rightNow;
 			self.status(self.STATUS_OK);
+			self.createMessage(rightNow, isoDate, 'start');
 		} else {
-			//console.log(result);
 			self.status(self.STATUS_OK);
 		}
 	},extra_headers);
